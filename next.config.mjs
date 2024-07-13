b8aeb92741079d159e03e2e996e6668708cd36b9
Nicolas RICHARD <invisiman042@gmail.com>
2024-07-13 11:54:15 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   images: {
     domains: ["localhost"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/serverApi/:path*",
-        destination: "http://localhost:5000/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
